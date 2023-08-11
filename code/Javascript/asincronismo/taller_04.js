005cb33d4d162f99e3541efa7846684b8d6767c5
Nathalie Estrella <nathaliebestrellamejia@gmail.com>
2023-08-10 20:01:23 -0500

    promises

---
+++ code/Javascript/asincronismo/taller_04.js
@@ -0,0 +1,31 @@
+const saludar = (nombre) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log(`Hola ${nombre}.`)
+            resolve(nombre)
+        }, 1000) 
+    })
+}
+
+const hablar = (nombre) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log(`Bla, bla, bla...`)
+            resolve(nombre)
+        }, 1000) 
+    })
+}
+
+const adios = (nombre) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log(`Terminando conversacion...`)
+            resolve(nombre)
+        }, 1000) 
+    })
+}
+
+console.log('Iniciando conversacion...')
+saludar('Nathalie')
+    .then( (dato) => hablar( dato ) )
+    .then( (dato) => adios( dato ) )
