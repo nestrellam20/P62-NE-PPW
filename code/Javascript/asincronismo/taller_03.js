8b0584ac3955c481f83f6df94ebf3aac4d515509
Nathalie Estrella <nathaliebestrellamejia@gmail.com>
2023-08-10 19:45:34 -0500

    final

--- code/Javascript/asincronismo/taller_03.js
+++ code/Javascript/asincronismo/taller_03.js
@@ -1,15 +1,22 @@
 function saludar(nombre, fn) {
     setTimeout(function() {
         console.log(`Hola ${nombre}`)
-    }, 1000)
+        fn(nombre)
+    }, 3000)
 }
 
 function hablar(nombre, fn) {
     setTimeout(function(){
-        
+        console.log('bla, bla, bla...')
+    }, 1000)
+}
+
+function adios(nombre) {
+    setTimeout(function(){
+        console.log(`Adios ${nombre}`)
+        console.log('Terminando conversacion...')
     }, 1000)
 }
 
 console.log('Iniciando conversacion...')
 saludar('Guillermo', hablar)
-console.log('Terminando conversacion...')
